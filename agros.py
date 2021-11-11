a9ec11cdef300c3e51e55edef2ae4df1439d082a
ElizaLastra <66333533+ElizaLastra@users.noreply.github.com>
2021-11-11 02:39:48 -0500

    Update agros.py

--- agros.py
+++ agros.py
@@ -26,7 +26,11 @@ def load_data():
 # 		data[col] = label.fit_transform(data[col])
 	return data
 
-df = load_data()   
+df = load_data() 
+df['Participante'] = 1
+
+df.groupby(['ASISTENCIA']).sum().plot(kind='pie', y='Participante')
+
 asistencia = df['ASISTENCIA'].unique()
 cond_asistencia = st.selectbox('Asistencia', asistencia)
 df[df['ASISTENCIA'] == cond_asistencia]
