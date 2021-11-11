ab1dc6363378f07b5217bfe472e202b84c2ad3b1
ElizaLastra <66333533+ElizaLastra@users.noreply.github.com>
2021-11-11 02:14:09 -0500

    Update agros.py

--- agros.py
+++ agros.py
@@ -4,13 +4,12 @@ import pandas as pd
 st.title('W1 Bootcamp DS')
 st.header('Inscripción en el evento de AGROS')
 
-@st.cache
-def get_data():
-    return pd.read_csv('https://github.com/ElizaLastra/repo/blob/master/base.csv', encoding = 'latin1', error_bad_lines=False)
+# @st.cache
+# def get_data():
+#     return pd.read_csv('https://github.com/ElizaLastra/repo/blob/master/base.csv', encoding = 'latin1', error_bad_lines=False)
 
-df = get_data()  
+# df = get_data()  
 
-asistencia = df['ASISTENCIA'].unique()
 
 # cond_asistencia = st.selectbox('Asistencia', asistencia)
 # df[df['ASISTENCIA'] == cond_asistencia]
@@ -18,3 +17,14 @@ asistencia = df['ASISTENCIA'].unique()
 #df.groupby(['ASISTENCIA']).value_counts().plot(kind='pie', y='ID')
 #df= pd.read_csv('https://github.com/ElizaLastra/repo/blob/master/base.csv', encoding = 'utf8')
 #df.groupby(['ASISTENCIA']).count().plot(kind='pie', y='ID')
+
+@st.cache(persist=True)
+	def load_data():
+		data = pd.read_csv('mushrooms.csv')
+		label = LabelEncoder()
+		for col in data.columns:
+			data[col] = label.fit_transform(data[col])
+		return data
+   
+ asistencia = data['ASISTENCIA'].unique()
+   
