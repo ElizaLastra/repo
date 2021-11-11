f11ce2f7ba05fbbe616b12c3b4a3570edd3b05e8
ElizaLastra <66333533+ElizaLastra@users.noreply.github.com>
2021-11-11 01:18:35 -0500

    Update agros.py

--- agros.py
+++ agros.py
@@ -4,5 +4,9 @@ import pandas as pd
 st.title('W1 Bootcamp DS')
 st.header('Inscripción en el evento de AGROS')
 
-df= pd.read_csv('https://github.com/ElizaLastra/repo/blob/master/base.csv', encoding = 'utf8')
+@st.cache
+def get_data():
+    return pd.read_csv('https://github.com/ElizaLastra/repo/blob/master/base.csv')
+  
+#df= pd.read_csv('https://github.com/ElizaLastra/repo/blob/master/base.csv', encoding = 'utf8')
 #df.groupby(['ASISTENCIA']).count().plot(kind='pie', y='ID')
