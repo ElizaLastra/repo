c8c0475c6dd46bb96e321a9c7e43c29411adfd13
ElizaLastra <66333533+ElizaLastra@users.noreply.github.com>
2021-11-11 02:50:47 -0500

    Update agros.py

--- agros.py
+++ agros.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt 
+from matplotlib.pyplot import pie, axis, show
+%matplotlib inline
 
 st.title('W1 Bootcamp DS')
 st.header('Inscripción en el evento de AGROS')
