79ba432bd552d85bf9cb3f70f77aded024144f21
ElizaLastra <66333533+ElizaLastra@users.noreply.github.com>
2021-11-11 00:23:36 -0500

    Update agros.py

--- agros.py
+++ agros.py
@@ -4,4 +4,5 @@ import numpy as np
 import plotly.figure_factory as ff
 import matplotlib.pyplot as plt
 
-st.title("W1 Bootcamp DS")
+st.title('W1 Bootcamp DS')
+st.header('Inscripción en el evento de AGROS')
