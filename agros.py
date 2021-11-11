76ba0ca49b43eedf48c7c57826538b69dd86fda3
ElizaLastra <66333533+ElizaLastra@users.noreply.github.com>
2021-11-11 09:18:22 -0500

    Update agros.py

--- agros.py
+++ agros.py
@@ -63,11 +63,13 @@ df1=df.groupby(['ASISTENCIA'])['ID'].count()
 fig4 = df1.plot(kind='pie',title='Asitencia al evento')
 st.pyplot(fig4)
 
-asistencia = df1["ASISTENCIA"]
-ctd = df1["ID"]
-# colors = ["#1f77b4", "#ff7f0e"]
-# explode = (0.1, 0, 0, 0, 0)  
-plt.pie(ctd, labels=asistencia,
-autopct='%1.1f%%')
-plt.title("Porcentaje de participación en el evento")
-plt.show()
+# asistencia = df1["ASISTENCIA"]
+# ctd = df1["ID"]
+# # colors = ["#1f77b4", "#ff7f0e"]
+# # explode = (0.1, 0, 0, 0, 0)  
+# plt.pie(ctd, labels=asistencia,
+# autopct='%1.1f%%')
+# plt.title("Porcentaje de participación en el evento")
+# plt.show()
+
+st.metric(label="Inscritos", value="1mil", delta="1.2")
