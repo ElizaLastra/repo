d6ee72f802586c560b90ba56c0c73196c754cef0
ElizaLastra <66333533+ElizaLastra@users.noreply.github.com>
2021-11-11 13:48:31 -0500

    Update agros.py

--- agros.py
+++ agros.py
@@ -59,9 +59,9 @@ st.header('Canal de inscripción al evento')
 cond_asistencia1= st.multiselect('Seleccione de acuerdo a asistencia', df1["ASISTENCIA"])
 df3 = df[(df['ASISTENCIA'].isin(cond_asistencia1))]
 df3= df3[['ID','ETIQUETA_CANAL']].groupby(['ETIQUETA_CANAL'], as_index=False).aggregate({'ID':'count'})
-fig1 = px.bar(df3, x='ETIQUETA_CANAL', y='ID',labels={'ETIQUETA_CANAL':'Motivación','ID':'Número de personas'})
-fig1.update_layout(title_text='Número de personas inscritas por canal')
-st.plotly_chart(fig1)
+fig3 = px.bar(df3, x='ETIQUETA_CANAL', y='ID',labels={'ETIQUETA_CANAL':'Canal','ID':'Número de personas'})
+fig3.update_layout(title_text='Número de personas inscritas por canal')
+st.plotly_chart(fig3)
 
 # st.write("""### 1. Data overview""",df.head())
 # st.write("""### 2. Aplicación de filtros""")
